cfa0bda7785f61165f96870ad37502b2b556c121
Matias Pierri <matiaspierri60@gmail.com>
2023-10-08 19:28:13 -0300

    encapsule in function

--- top_10_most_mentioned_users.py
+++ top_10_most_mentioned_users.py
@@ -1,50 +1,54 @@
 import pandas as pd
 import json
-#from memory_profiler import profile
+import cProfile
+from memory_profiler import profile
 
-# Specify the file path
-file_path = "farmers-protest-tweets-2021-2-4.json"
+@profile
+def find_top_10_most_mentioned_users(file_path: str):
 
-data= []
-invalid_rows= []
+    # Specify the file path
+    file_path = "farmers-protest-tweets-2021-2-4.json"
 
-missing_rows = 0
+    data= []
+    invalid_rows= []
 
-# Try to read the JSON file
-with open(file_path, 'r', encoding='utf-8') as file:
-    # Read and parse each line separately
-    for row in file:
-        try:
-            jsonparse = json.loads(row)
-            data.append(jsonparse)
-        except Exception as e:
-            missing_rows +=1
-            invalid_rows.append(row)
+    missing_rows = 0
 
-print(f'Missing rows: {missing_rows}')
-print(f'Invalid rows: {invalid_rows}')
+    # Try to read the JSON file
+    with open(file_path, 'r', encoding='utf-8') as file:
+        # Read and parse each line separately
+        for row in file:
+            try:
+                jsonparse = json.loads(row)
+                data.append(jsonparse)
+            except Exception as e:
+                missing_rows +=1
+                invalid_rows.append(row)
 
-# Create a DataFrame from the parsed data
-df = pd.DataFrame(data)
+    print(f'Missing rows: {missing_rows}')
+    print(f'Invalid rows: {invalid_rows}')
+
+    # Create a DataFrame from the parsed data
+    df = pd.DataFrame(data)
 
 
-# Drop rows with None or NaN values in the "mentionedUsers" column
-# We only want to keep users that were mentioned
-df.dropna(subset=['mentionedUsers'], inplace=True)
+    # Drop rows with None or NaN values in the "mentionedUsers" column
+    # We only want to keep users that were mentioned
+    df.dropna(subset=['mentionedUsers'], inplace=True)
 
-df['usernameMentionedList'] = df['mentionedUsers'].apply(lambda user_list: [user['username'] for user in user_list if 'username' in user])
+    df['usernameMentionedList'] = df['mentionedUsers'].apply(lambda user_list: [user['username'] for user in user_list if 'username' in user])
 
-# Create a dictionary that contains the count of each username
-username_mention_count = {}
+    # Create a dictionary that contains the count of each username
+    username_mention_count = {}
 
-for username in username_mentioned_plain_string.split():
-    if username in username_mention_count:
-        username_mention_count[username] += 1
-    else:
-        username_mention_count[username] = 1
+    for username in username_mentioned_plain_string.split():
+        if username in username_mention_count:
+            username_mention_count[username] += 1
+        else:
+            username_mention_count[username] = 1
 
-# Transform the dictionary into a list of tuples, ordered by descending count
-sorted_username_tuples = sorted(username_mention_count.items(), key=lambda x: x[1], reverse=True)
+    # Transform the dictionary into a list of tuples, ordered by descending count
+    sorted_username_tuples = sorted(username_mention_count.items(), key=lambda x: x[1], reverse=True)
 
-# Keep only top 10
-sorted_username_tuples[:10]
+    # Keep only top 10
+    return tuple(sorted_username_tuples[:10])
