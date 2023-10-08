05ae8ff9a118374cc11703e0e4e2060e116eae17
Matias Pierri <matiaspierri60@gmail.com>
2023-10-08 11:47:34 -0300

    extract emoji function

--- top_10_used_emojis.py
+++ top_10_used_emojis.py
@@ -3,3 +3,9 @@ import json
 import cProfile
 import emoji
 from memory_profiler import profile
+
+
+# Define a function to extract emojis from a string using the emoji module
+def extract_emojis(text):
+    return ''.join(char for char in text if emoji.is_emoji(char))
+
